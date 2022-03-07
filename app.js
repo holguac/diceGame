7c8c6f849b5f0feb64545c0c1bf688ee4a2d3686
Holly Christopher <holly_christopher@hotmail.com>
2022-03-07 19:26:29 +0000

    third commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const body = document.getElementById("game");
-const imageContainer = document.querySelector(".image-container");
-const scoreContainer = document.querySelector(".score-container");
+const imageContainer = document.querySelector(".imageContainer");
+const scoreContainer = document.querySelector(".scoreContainer");
 
 const scorePlayer1 = document.getElementById("scorePlayer1");
 const imagePlayer1 = document.getElementById("imagePlayer1");
@@ -21,31 +21,18 @@ rollPlayer1.addEventListener("click", () => {
 	scorePlayer1.textContent = player1Total;
 
 	if (player1Total >= 20) {
-		scorePlayer1.textContent = `${player1Total} - Winner!`;
-		// scorePlayer1.style.fontWeight = "bold";
-		// imageContainer.style.backgroundColor = "#00af7a";
-		// scoreContainer.style.backgroundColor = "#00af7a";
-		// body.style.backgroundColor = "#00C288";
+		scorePlayer1.textContent = `${player1Total} WINNER!`;
+		scorePlayer1.style.fontWeight = "bold";
 
 		player1Total = 0;
-
-		// rollPlayer1.style.display = "none";
-		// playAgain.style.display = "block";
-		// playAgain.style.backgroundColor = "#00885f";
 		
 	} else {
 		if (currentRoll == 1) {
-			scorePlayer1.textContent = `${player1Total} - You lost!`;
-			// scorePlayer1.style.fontWeight = "bold";
-			// imageContainer.style.backgroundColor = "#990033";
-			// scoreContainer.style.backgroundColor = "#990033";
-			// body.style.backgroundColor = "#AA0039";
+			scorePlayer1.textContent = `${currentRoll} YOU LOST!`;
+			scorePlayer1.style.fontWeight = "bold";
 
 			player1Total = 0;
-			
-			// rollPlayer1.style.display = "none";
-			// playAgain.style.display = "block";
-			// playAgain.style.backgroundColor = "#770028";
+
 		}
 	}
 });
